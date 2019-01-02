7636a0305e16449441df904b9f5241f880d5b3f8
Leza Lutonda <lemol-c@hotmail.com>
2019-01-02 16:47:40 +0100

    add example project

---
+++ example/.umirc.js
@@ -0,0 +1,20 @@
+
+// ref: https://umijs.org/config/
+export default {
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    ['umi-plugin-react', {
+      antd: false,
+      dva: false,
+      dynamicImport: true,
+      title: 'example',
+      dll: true,
+      hardSource: true,
+      routes: {
+        exclude: [
+          /components/,
+        ],
+      },
+    }],
+  ],
+}
